79ff35dec4a9b46f74f5b3a3cebbbd77736098f9
Hector <adeniyi.olaitanhector@yahoo.com>
2024-01-26 22:32:22 +0100

    Add AOS library and initialize it in useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Components/Pages/Landing-Page/LandingPage';
 import Footer from './Components/Footer/Footer';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+  }, [])
   return (
     <div className="App">
     <Navbar />
